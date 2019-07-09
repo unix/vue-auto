dc2e65e584b8c2b73935d0ea8a147d751aa5b215
WittBulter <lambdas.dev@gmail.com>
2019-07-09 14:58:46 +0800

    fix: remove duplicate prefixs

--- src/component.js
+++ src/component.js
@@ -3,7 +3,7 @@ import * as util from './util'
 export const createComponents = (vue, components) => {
   components
     .forEach(com => {
-      if (!com.routerName.startsWith(util.pagePrefix)) {
+      if (!com.routerName.startsWith(util.pagePrefix) && !com.routerName.startsWith(util.getPrefix())) {
         com.routerName = `${util.getPrefix()}-${com.routerName}`
       }
       com.name = com.name || com.routerName
