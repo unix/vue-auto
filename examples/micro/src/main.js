017e2b95d31b5d059bc5a868de2a7d1f5582c18d
WittBulter <lambdas.dev@gmail.com>
2019-07-09 14:59:22 +0800

    chore: upgrade deps

--- examples/micro/src/main.js
+++ examples/micro/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import app from './app.vue'
 import { install } from 'vue-auto'
 
-install(Vue)
+install(Vue, {
+  autoRouter: false,
+})
+
 new Vue({
   render: h => h(app)
 }).$mount('#app')
